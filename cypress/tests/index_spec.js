c073d7e315b49e1792785efb094b0d69321a0934
Brandon Brown <brandon@brandonb.ca>
2019-07-12 09:30:23 -0700

    Upgrades Cypress to clear vulnerability warnings (#114)
    * Upgrades Cypress to 3.4.0, clearing open vulnerabilities
    
    * Fix the tests to actually pass

--- cypress/tests/index_spec.js
+++ cypress/tests/index_spec.js
@@ -1,12 +1,25 @@
-const qaAttr = (selector) => {
-  return `[data-cy="${selector}"]`
-}
-
 describe('First Battlesnake test', () => {
-  it('should show the page to login', () => {
+  before(() => {
+    // Hide the Django toolbar if it's showing
+    // and move it down the page so it doesn't block any elements
+    cy.setCookie('djdt', 'hide')
+    cy.setCookie('djdttop', '600')
+  })
+
+  beforeEach(() => {
+    Cypress.Cookies.preserveOnce('djdt', 'djdttop')
+
     cy.visit('/')
+  })
+
+  it('should show the page to login', () => {
     cy.getCookie('sessionid').should('not.exist')
+    cy.get('a')
+      .contains('Login')
+      .click()
     cy.url().should('contain', '/login/')
-    cy.get(qaAttr('button-login')).should('have.attr', 'href', '/oauth/login/github/')
+    cy.get('a')
+      .contains('Sign in with GitHub')
+      .should('have.attr', 'href', '/oauth/login/github/?next=')
   })
 })
