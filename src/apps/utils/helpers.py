db2baed093067c7ae92978932b3eeb47dd7e48b1
Brad Van Vugt <1531419+bvanvugt@users.noreply.github.com>
2019-08-15 21:15:39 -0700

    Use new settings for Board and Engine URLs.

--- src/apps/utils/helpers.py
+++ src/apps/utils/helpers.py
@@ -9,8 +9,8 @@ def generate_game_url(game: Game):
     if game.engine_url is not None and len(game.engine_url) > 0:
         engine_url = game.engine_url
 
-    return f"{settings.BOARD_URL}/?engine={urlquote(engine_url)}&game={game.engine_id}"
+    return f"{settings.BATTLESNAKE_BOARD_URL}/?engine={urlquote(engine_url)}&game={game.engine_id}"
 
 
 def generate_exporter_url(engine_id):
-    return f"{settings.EXPORTER_URL}/games/{engine_id}/gif"
+    return f"{settings.BATTLESNAKE_EXPORTER_URL}/games/{engine_id}/gif"
