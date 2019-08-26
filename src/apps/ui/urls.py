5f816908818ef0c2b8af9d928da093d6d167d6b0
Brad Van Vugt <1531419+bvanvugt@users.noreply.github.com>
2019-08-26 14:27:18 -0700

    Correct docs links.

--- src/apps/ui/urls.py
+++ src/apps/ui/urls.py
@@ -11,7 +11,7 @@ from apps.ui.views import public
 
 class EXTERNAL_URLS:
     BLOG = "https://medium.com/battlesnake"
-    DOCS = "https://docs.battlesnake.io"
+    DOCS = "https://docs.battlesnake.com"
     GITHUB = "https://github.com/battlesnakeio"
     HIGHLIGHTS = "https://www.youtube.com/watch?v=d9ARbSzBKxc"
     SLACK = "https://join.slack.com/t/battlesnake/shared_invite/enQtNzM4NDQ3MjgyMjI0LWJkZGJkOTg3NTAyNjg2MWVhMzk5OTVlMjk2ZjIzMWUxMWQ3MzYxN2I4YTY4YTE0YTI0MmQ4MzdiODNiZTgyZGE"
