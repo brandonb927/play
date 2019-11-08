c1b44a000d7288a9f3f660411d33c0c18b2e6586
Brad Van Vugt <1531419+bvanvugt@users.noreply.github.com>
2019-11-07 21:01:20 -0800

    Add events.Team model, add events.Event.slug

--- src/apps/events/admin.py
+++ src/apps/events/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from apps.events.models import Event
+from apps.events.models import Event, Team
 from apps.common.models import BaseModelAdmin
 
 
@@ -9,3 +9,10 @@ class EventAdmin(BaseModelAdmin):
     readonly_fields = BaseModelAdmin.readonly_fields + ("id",)
     list_display = ("name", "date", "location", "is_listed")
     ordering = ("date",)
+
+
+@admin.register(Team)
+class TeamAdmin(BaseModelAdmin):
+    readonly_fields = BaseModelAdmin.readonly_fields + ("id",)
+    list_display = ("name", "event")
+    ordering = ("name",)
--- src/apps/events/models.py
+++ src/apps/events/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 from apps.common.fields import ShortUUIDField
 from apps.common.models import BaseManager, BaseModel
+from apps.core.models import Account, Snake
 import util.time
 
 
@@ -19,6 +20,7 @@ class Event(BaseModel):
     id = ShortUUIDField(prefix="evt", max_length=128, primary_key=True)
 
     name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
     description = models.TextField(
         default="", blank=True, help_text="This field supports Markdown."
     )
@@ -33,3 +35,17 @@ class Event(BaseModel):
 
     def is_upcoming(self):
         return not self.date or self.date >= util.time.today()
+
+
+class Team(BaseModel):
+    id = ShortUUIDField(prefix="team_", max_length=128, primary_key=True)
+
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+
+    name = models.CharField(max_length=100)
+    accounts = models.ManyToManyField(Account)
+    snake = models.ForeignKey(Snake, on_delete=models.DO_NOTHING)
+
+    bio = models.TextField(default="", blank=True)
+    profile_pic_url = models.URLField(default="", blank=True)
+    profile_pic_approved = models.BooleanField(default=False)
